1a578e95cf22974663c1e5e3fc435ebcb83b2842
Pedro Sandoval <psandovalsegura@gmail.com>
2019-03-30 17:35:27 -0700

    States display

--- prologtikz.py
+++ prologtikz.py
@@ -1,16 +1,82 @@
 #
 # Script to convert prolog automata to tikz code
 #
+
 import argparse
+import re
+
+BOILERPLATE_start = "% Author: Till Tantau\n \
+% Source: The PGF/TikZ manual\n \
+\\documentclass{article}\n \
+\\usepackage{pgf}\n \
+\\usepackage{tikz}\n \
+\\usetikzlibrary{arrows,automata}\n \
+\\usepackage[latin1]{inputenc}\n \
+\\begin{document}\n \
+\\begin{tikzpicture}[->,>=stealth',shorten >=1pt,auto,node distance=2.8cm, semithick]\n \
+    \\tikzstyle{every state}=[fill=red,draw=none,text=white]\n \
+    \\node[initial,state] (A0)                    {$q_0$};\n"
+
+BOILERPLATE_end = "\\end{tikzpicture}\n \
+\\end{document}"
+
+accept_re = r"accepting\((\w*)\)."
+transition_re = r"transition\((\w*), (\w*), (\w*)\)."
+
+STATES = [] # Strings
+EDGES = []  # Tuples 
+STATES_TO_TIKZSTATE = {}
+
+def insertState(statename):
+    if statename not in STATES:
+        STATES.append(statename)
+
+def getStatesAndTransitions(f):
+    for line in f.readlines():
+        matched_accept = re.match(accept_re, line)
+        if matched_accept:
+            groups = matched_accept.groups()
+            state = groups[0]
+            insertState(state)
+
+        matched_transition = re.match(transition_re, line)
+        if matched_transition:
+            groups = matched_transition.groups()
+            state1, symbol, state2 = groups
+            insertState(state1)
+            insertState(state2)
+            EDGES.append((state1, symbol, state2))
+
+    # Remove q0 state (always assumed to be the start state)
+    STATES.remove("q0")
+    STATES_TO_TIKZSTATE["q0"] = "A0"
+
+def getTikzNodeCode():
+    c = ""
+    for i, state in enumerate(STATES):
+        tikz_state = f"A{i+1}"
+        if i == 0:
+            c += f"\\node[state]         ({tikz_state})[right of=A0] {{${state}$}};\n"
+        else:
+            c += f"\\node[state]         ({tikz_state})[right of=A{i}] {{${state}$}};\n"
+    return c
 
 def main():
     parser = argparse.ArgumentParser(description='Convert prolog code to tikz')
     parser.add_argument('prolog_file', type=argparse.FileType('r'),
                                        help='Prolog file with automata definition')
+    parser.add_argument('output_filename', help='Output filename')
     args = parser.parse_args()
 
-    with args.prolog_file as f:
-        print(f.read())
+    getStatesAndTransitions(args.prolog_file)
+    print(STATES)
+
+    tikzNodeCode = getTikzNodeCode()
+
+    print(tikzNodeCode)
+
+    with open(args.output_filename, "w") as f:
+        print(BOILERPLATE_start + tikzNodeCode + BOILERPLATE_end, file=f)
 
 if __name__ == '__main__':
     main()
---
+++ tests/dfa1.pl
@@ -0,0 +1,23 @@
+
+%% The start state is understood to ALWAYS be named q0
+accepting(q0).
+
+
+
+transition(q0, 0, q1).
+transition(q1, 0, q0).
+
+transition(q2, 0, q3).
+transition(q3, 0, q2).
+
+transition(q4, 0, q5).
+transition(q5, 0, q4).
+
+transition(q0, 1, q2).
+transition(q2, 1, q4).
+
+transition(q4, 1, q0).
+
+transition(q1, 1, q3).
+
+transition(q3, 1, q5).
